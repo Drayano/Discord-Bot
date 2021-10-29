4dac952d144fa772515aa1a61f3f36329640c6bf
yzdbg <yazid.bouguedra@gmail.com>
2021-10-29 13:03:41 +0100

    Spongebob command now also displays a gif

---
+++ environment.d.ts
@@ -0,0 +1,18 @@
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        DISCORDJS_BOT_TOKEN: string,
+        DISCORDJS_TESTBOT_TOKEN: string,
+        DISCORDJS_BOT_ID: string,
+        DISCORDJS_TESTBOT_ID: string,
+        GUILD_ID_PLAYGROUND: string,
+        GUILD_ID_YUGEN: string,
+        MEGA_DRIVE_SID: string,
+        EMPLOIS_SID: string,
+        EMPLOIS_IA: string,
+        SPONGEBOB_GIF: string
+      }
+    }
+  }
+  
+export { };
