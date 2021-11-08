0beaa99c9de4a952888bf9b04829fa0db29ece66
Drayano <houari.rezk.1999@gmail.com>
2021-11-08 19:52:20 +0100

    another typo fix

--- ts/Commands/help.ts
+++ ts/Commands/help.ts
@@ -16,16 +16,16 @@ export async function command_help(discord_client: Client, interaction: Interact
         const embed = new MessageEmbed()
             .setTitle(`${discord_client.user?.username} BOT Help`)
             .setDescription(`These are all the available commands for ${discord_client.user?.username} BOT on ${interaction.guild?.name} : \n
-            /emplois_sid : Shows the schedule for SID. Channels : ${yugen_etudes} or ${yugen_resources} \n
-            /emplois_ia : Shows the schedule for IA. Channels : ${yugen_etudes} or ${yugen_resources} \n
-            /drive : Shows the Mega Drive link for SID. Channels : ${yugen_etudes} or ${yugen_resources} \n
+            /emplois_sid : Shows the schedule for SID. Channels : #${yugen_etudes} or #${yugen_resources} \n
+            /emplois_ia : Shows the schedule for IA. Channels : #${yugen_etudes} or #${yugen_resources} \n
+            /drive : Shows the Mega Drive link for SID. Channels : #${yugen_etudes} or #${yugen_resources} \n
             /code : Shows the link to the Bot source code \n
             /spongebob [input] : Transforms the input text into sPoNgE bOb cAsE \n
-            /xkcd : Post a random XKCD Comic. Channel : ${yugen_xkcd} \n
+            /xkcd : Post a random XKCD Comic. Channel : #${yugen_xkcd} \n
             /memes [input] [first_line] [second_line] : Create a meme from the input using the lines as captions
             Eg : /memes input:drake first_line:nice second_line:meme
             To create a Drake no/yes meme with the captions "nice" and "meme"
-            Channel : ${yugen_memes} \n
+            Channel : #${yugen_memes} \n
             /translate [target] [input] : Translate the input text into the targeted language. 
             Target needs to be in ISO 639-1 form (i.e : fr, en, es, de, ja etc...)`);
 
